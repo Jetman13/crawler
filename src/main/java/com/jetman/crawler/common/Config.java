493fce3e4169cac3b7ba6d0c0bf85e1c558f54ee
Jetman13 <823099993@qq.com>
2016-06-10 10:49:35 +0800

    增加配置文件window和linux的不同目录

--- src/main/java/com/jetman/crawler/common/Config.java
+++ src/main/java/com/jetman/crawler/common/Config.java
@@ -2,6 +2,13 @@ package com.jetman.crawler.common;
 
 public class Config {
 	
-	public static final String path = "E://crawlerdown";
+	public static String path = "E://crawlerdown";
+	
+	static {
+		System.out.println("file separator = "+java.io.File.separator);
+		if (java.io.File.separator.equals("/")) {
+			path = "/home/jetman/crawlerdown";
+		}
+	}
 
 }
